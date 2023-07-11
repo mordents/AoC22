d4356cb8e1daeeb22d91c092ccedaabe610dd198
Isobel_Wilson <isobel.wilson@thedatashed.co.uk>
2023-07-11 14:55:37 +0100

    added text file

--- AoC1.py
+++ AoC1.py
@@ -1,1 +1,7 @@
-print("Hello, world NOW")
+# The Elves document snack calories# 
+# For example, suppose the Elves finish writing their items' Calories and end up with the following list:
+# Separated by blank lines - add 
+# Find the Elf carrying the most Calories. How many total Calories is that Elf carrying? 
+import sys 
+with open(input.txt) as input:
+    lines = [line for line in input]
