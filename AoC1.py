8dfbe9ae5b4fbd903d2c7992600fb858bd125de5
Isobel_Wilson <isobel.wilson@thedatashed.co.uk>
2023-07-11 15:59:13 +0100

    solved the elves

--- AoC1.py
+++ AoC1.py
@@ -2,7 +2,20 @@
 # For example, suppose the Elves finish writing their items' Calories and end up with the following list:
 # Separated by blank lines - add 
 # Find the Elf carrying the most Calories. How many total Calories is that Elf carrying? 
-import sys 
-with open(input.txt) as input:
-    lines = [line for line in input]
-print(lines)
+import re 
+with open('input.txt', 'r') as input:
+    lines= input.readlines()                        #read in all lines#
+    calories = [line.strip() for line in lines]    #remove trailing input 
+
+total=[]
+sum = 0
+for x in calories:
+    if x!="":
+        sum+=int(x)
+    elif x=="":
+        total.append(sum)
+        sum=0
+totalsort=sorted(total,reverse=True)        
+sum2=totalsort[0]+totalsort[1]+totalsort[2]
+print(sum2)
+# add things in between  /n blank /n #
