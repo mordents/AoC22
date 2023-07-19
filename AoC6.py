e9590b70b7e0ea8e7da0e3aa787e20232c7ac82a
Isobel_Wilson <isobel.wilson@thedatashed.co.uk>
2023-07-19 07:14:16 +0100

    some items added

--- AoC6.py
+++ AoC6.py
@@ -1,19 +1,8 @@
-#signal is from seemingly random characters 
-#add a subroutine that detects a start of packet marker - four chars that are all differ#
-# The device will send your subroutine a datastream buffer (your puzzle input); your subroutine needs to identify the first position where the four most recently received characters were all different. 
-#Specifically, it needs to report the number of characters from the beginning of the buffer to the end of the first such four-character marker.
-# For example, suppose you receive the following datastream buffer:
-# mjqjpqmgbljsphdztnvjfqwrcgsmlb
-# After the first three characters (mjq) have been received, there haven't been enough characters received yet to find the marker. The first time a marker could occur is after the fourth character is received, making the most recent four characters mjqj. Because j is repeated, this isn't a marker.
-# The first time a marker appears is after the seventh character arrives. Once it does, the last four characters received are jpqm, which are all different. In this case, your subroutine should report the value 7, because the first start-of-packet marker is complete after 7 characters have been processed.
-# Here are a few more examples:
-
-# bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
-# nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
-# nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
-# zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
-# How many characters need to be processed before the first start-of-packet marker is detected?
-for char in input():
-    if char.isunique():
-        pass 
-        
+with open('AoC6input.txt','r') as f: #cooooolll
+    myinput =f.readline().strip()
+    
+def read_idx(myinput):
+    for i in range(len(myinput)):
+        if len(set(myinput[i:i+14]))==14: #go through input , find the index where set is 4 chars
+            answer=i+14     # get that idx +4 - last unique idx
+            return answer
