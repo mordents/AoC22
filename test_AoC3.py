2a3d4d5cbf6038e049682855d6c5bc249dac7201
Isobel_Wilson <isobel.wilson@thedatashed.co.uk>
2023-07-24 13:27:55 +0100

    added AoC3

---
+++ test_AoC3.py
@@ -0,0 +1,14 @@
+#load backpack with supplies - each backpack has 2 comp# 
+# all items of x type are meant to go into 1 of the two compartments#
+#one item type per backpack has been misclassified#
+#list of all items currently in rucksack#
+#each type identified by a single uppercase or lowercase letter#
+#e.g. a vs A - different types of items #
+#same number of items in each compartment for each backpack#
+
+#sample input#
+#find the item type(s) that appear(s) in both compt. of each backpack#
+#what is the sum of the priorities of those item types 
+import pytest   
+def test_splitline():
+    pass 
